2e7a7c40017f68b82274224f682f7e4a2280e11c
Milton <miltonsoftwareengineer@gmail.com>
2022-10-28 10:37:37 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+# Your code goes here!
+class Anagram
+    def initialize(keyword)
+        @keyword = keyword
+    end
+    def match(words)
+        match = []
+        for keyword in words do
+            if(keyword.chars.sort.join==@keyword.chars.sort.join)
+                match << keyword
+            end
+        end
+        match
+    end
+end
